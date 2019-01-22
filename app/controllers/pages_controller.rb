e35997f49cfdd7922632773980929588fcf66fe8
Merel <merel.steenbrink@gmail.com>
2019-01-22 11:44:36 +0100

    add some colors and gifs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,11 +6,15 @@ class PagesController < ApplicationController
   	@question =  params[:question]
   	if @question == "Let's dance!"
   		@answer = "YES! Let's dance!"
+  		@link = "https://giphy.com/embed/3rgXBQIDHkFNniTNRu"
   	elsif @question[-1] === "?"
   		@answer = "Let's find out dancing!"
+  		@link = "https://giphy.com/embed/5wWf7H0WTquIU1DFY4g"
   	else
   		@answer = "OK, but now: Let's dance!"
+  		@link = "https://giphy.com/embed/BkL4Vyz0z2iYQMhwFw"
   	end
   
   end
 end
+
