fc1c47440db576e7ee09165d0882d04f3af9f879
Merel <merel.steenbrink@gmail.com>
2019-01-22 21:51:31 +0100

    change question to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'question', to: 'pages#question'
   get 'answer', to: 'pages#answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root to: 'pages#question'
 
 end
